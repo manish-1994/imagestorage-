d27ddfb22ab0c7d1897ac871898bbb09669d0080
manny-1994 <paramguru45@gmail.com>
2019-10-20 18:21:41 +0530

    updated files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {AngularFireStorage, AngularFireStorageReference} from "@angular/fire/storage";
+import { AngularFireUploadTask } from "@angular/fire/storage";
+import {finalize, map, tap} from "rxjs/operators";
+import {Observable} from "rxjs";
+import {async} from "rxjs/internal/scheduler/async";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'uploading';
+  ref: AngularFireStorageReference;
+  task: AngularFireUploadTask;
+  uploadProgress: Observable<number>;
+  uploadState: Observable<string>;
+  img: any;
+  snapshot:Observable<any>;
+  downloadurl:string;
+
+  constructor(private afStorage: AngularFireStorage) {
+  this.img = ''
+  }
+
+  upload(event) {
+    const id = Math.random().toString(36).substring(2);
+    this.ref = this.afStorage.ref(id);
+    this.task = this.ref.put(event.target.files[0]);
+    this.uploadState = this.task.snapshotChanges().pipe(map(s => s.state));
+    this.uploadProgress = this.task.percentageChanges();
+  this.snapshot = this.task.snapshotChanges().pipe(
+    tap(console.log),
+    finalize( async() => {
+       this.downloadurl = await this.ref .getDownloadURL().toPromise();
+    })
+  );
+
+  }
+  delete(){
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import {environment} from "../environments/environment";
+import { AngularFirestoreModule} from "@angular/fire/firestore";
+import {AngularFireStorageModule} from "@angular/fire/storage";
+import {FormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatFormFieldModule, MatProgressBarModule} from "@angular/material";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatProgressBarModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
